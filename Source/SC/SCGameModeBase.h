ed9712b836b96b3342e433dde318ae8aee02cc18
Velktri <murrayg226@gmail.com>
2017-08-05 16:04:54 -0700

    Initial commit

---
+++ Source/SC/SCGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SCGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SC_API ASCGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
